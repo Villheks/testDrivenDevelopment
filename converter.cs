3522d49fd323858684625613ebfe0654537e9da5
Ane Birgitte Haug Skoglund <aneskoglund@Anes-MacBook-Air.local>
2024-02-09 19:03:36 +0100

    empty tests

---
+++ converter.cs
@@ -0,0 +1,18 @@
+public class test 
+{
+    
+    public double InchesToMillimeters(double inches)
+    {
+        
+    }
+
+    public double InchesToCentimeters(double inches)
+    {
+        
+    }
+
+    public double InchesToMeters(double inches)
+    {
+        
+    }
+}
